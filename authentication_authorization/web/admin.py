30b6e49e5133ffc36dede7cf93bbfa539485fb50
Bogomila <bkatsarska@abv.bg>
2023-07-11 13:19:51 +0300

    Added User and Password Management - Part III

--- authentication_authorization/web/admin.py
+++ authentication_authorization/web/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from authentication_authorization.auth_app.views import SignUpForm
+
+UserModel = get_user_model()
+
+@admin.register(UserModel)
+class AppUserAdmin(UserAdmin):
+    ordering = ('email,')
+    list_display = ['email', 'date_joined', 'last_login']
+    list_filter = ()
+    add_form = SignUpForm
